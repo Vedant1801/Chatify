c5158a9d685afe226a13149b643ada04086a5270
Vedant Gupta <vedantguptajhansi18@gmail.com>
2026-01-18 21:59:22 +0530

    fix frontend static path

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,12 +20,22 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+// // make ready for deployment
+// if (ENV.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+//   app.get("*", (_, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 // make ready for deployment
 if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
   app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(
+      path.join(__dirname, "frontend", "dist", "index.html")
+    );
   });
 }
 
